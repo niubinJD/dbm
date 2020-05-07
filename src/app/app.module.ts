b2e1b938bf50d9d49b76f94b69e01ec33f8d9af1
niubin <niubinbing@gmail.com>
2020-05-07 23:39:00 +0800

    添加ng-zorro-antd作为ui框架

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,6 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HomeModule } from './home/home.module';
 
 import { AppComponent } from './app.component';
-
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
