c87495af7940464454060a87a8b384e643da3031
niubi <niubinbing@gmail.com>
2020-05-11 19:08:02 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ElectronService, DataStoreService } from './core/services';
 import { TranslateService } from '@ngx-translate/core';
 import { AppConfig } from '../environments/environment';
+import { DataBase } from './entity/database';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  data: Array<DataBase> = [];
   constructor(
     public electronService: ElectronService,
     private translate: TranslateService,
@@ -25,7 +27,11 @@ export class AppComponent {
       console.log('Mode web');
     }
   }
+  ngOnInit(): void {
+    // this.data = this.store.getDataStore('metadata').find({}) ;
+  }
 
+  update(){
 
-  update(){}
+  }
 }
